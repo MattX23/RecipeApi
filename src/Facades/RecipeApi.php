897640104c0f8fc1d9aec6dc855c0976ffb2b56c
MattX23 <matt.wood001@gmail.com>
2020-02-28 21:50:28 +0000

    Add facade

--- src/Facades/RecipeApi.php
+++ src/Facades/RecipeApi.php
@@ -5,8 +5,8 @@ namespace MattX23\RecipeApi\Facades;
 
 use Illuminate\Support\Facades\Facade;
 /**
- * @method \MattX23\RecipeApi\Api\Recipes random
- * @method \MattX23\RecipeApi\Api\Recipes hello
+ * @method \MattX23\RecipeApi\RecipeApi random
+ * @method \MattX23\RecipeApi\RecipeApi hello
  */
 
 class RecipeApi extends Facade
