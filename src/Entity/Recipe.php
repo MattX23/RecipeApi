ab41b1c1d4e87ca459b85e7a5732046f1199b798
MattX23 <matt.wood001@gmail.com>
2020-02-29 22:44:37 +0000

    Limit data within recipe

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -3,16 +3,32 @@
 namespace MattX23\RecipeApi\Entity;
 
 use Illuminate\Support\Arr;
+use stdClass;
 
 class Recipe
 {
     /**
-     * @var array
+     * @var object
      */
     public $recipe;
 
     public function __construct($parameters = [])
     {
-        $this->recipe = Arr::get($parameters,'0');
+        $spoonacularRecipe = collect(Arr::get($parameters,'0'));
+
+        $recipe = new stdClass;
+
+        $recipe->cookingMinutes = Arr::get($spoonacularRecipe,'cookingMinutes');
+        $recipe->readyInMinutes = Arr::get($spoonacularRecipe,'readyInMinutes');
+        $recipe->glutenFree = Arr::get($spoonacularRecipe,'glutenFree');
+        $recipe->dairyFree = Arr::get($spoonacularRecipe,'dairyFree');
+        $recipe->vegetarian = Arr::get($spoonacularRecipe,'vegetarian');
+        $recipe->vegan = Arr::get($spoonacularRecipe,'vegan');
+        $recipe->extendedIngredients = Arr::get($spoonacularRecipe,'extendedIngredients');
+        $recipe->id = Arr::get($spoonacularRecipe,'id');
+        $recipe->imageType = Arr::get($spoonacularRecipe,'imageType');
+        $recipe->analyzedInstructions = Arr::get($spoonacularRecipe,'analyzedInstructions');
+
+        $this->recipe = $recipe;
     }
 }
